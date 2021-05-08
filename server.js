8aae4edb8d0e5387acc49c6f09ee21f762fa8f80
Hede <Heatherj.swift@gmail.com>
2021-05-08 17:38:33 -0400

    get /notes returns notes.html

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 //const apiRoutes = require('./routes/apiRoutes');
-//const htmlRoutes = require('./routes/htmlRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
@@ -14,7 +14,7 @@ app.use(express.static('public'));
 
 // use apiRoutes
 //app.use('/api', apiRoutes);
-//app.use('/', htmlRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
